1885ad66318e1410efdef852968a6d14b1654eef
WendyFDU <spritewen@gmail.com>
2017-01-01 19:43:03 +0800

    setup

---
+++ src/Equipment.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Ding on 17/1/1.
+ */
+public class Equipment {
+}
---
+++ src/EquipmentDescriptor.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Ding on 17/1/1.
+ */
+public class EquipmentDescriptor {
+    private String name;
+    private String descriptor;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescriptor() {
+        return descriptor;
+    }
+
+    public void setDescriptor(String descriptor) {
+        this.descriptor = descriptor;
+    }
+}
